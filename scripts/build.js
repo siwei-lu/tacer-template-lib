7520a8b394c2e90ba427fe7c45efeb827505e6cd
Idan Loo <siwei.lu@lusiweis-MacBook-Pro.local>
2019-07-03 17:01:10 +0800

    feat: generate declarations by default.

--- scripts/build.js
+++ scripts/build.js
@@ -8,12 +8,21 @@ const entrypoint = resolve(`${process.cwd()}/src/index.ts`)
 const distPath = resolve(`${process.cwd()}/dist/index.js`)
 const cacheRoot = resolve(__dirname, '../.cache')
 
+const tsconfigDefaults = {
+  declaration: true,
+  declarationDir: './types',
+}
+
 function build() {
   rollup({
     input: entrypoint,
     plugins: [
       autoExternal(),
-      typescript({ cacheRoot, useTsconfigDeclarationDir: true }),
+      typescript({
+        cacheRoot,
+        tsconfigDefaults,
+        useTsconfigDeclarationDir: true,
+      }),
       terser(),
     ],
   }).then(bundle => bundle.write({ file: distPath, format: 'cjs' }))
